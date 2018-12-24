f13a56f0d7121cae18c451438f9f72be81403028
Tyler Maschino <Tylermaschino@gmail.com>
2018-12-24 14:55:10 -0500

    Finished front end

---
+++ client/src/components/BookList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { getBooksQuery } from '../queries/queries';
+import BookDetails from './BookDetails';
+
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null,
+    };
+  }
+
+  displayBooks() {
+    let data = this.props.data;
+    return data.loading ? (
+      <div>Loading books...</div>
+    ) : (
+      data.books.map(book => (
+        <li
+          key={book.id}
+          style={{ cursor: 'pointer' }}
+          onClick={e => this.setState({ selected: book.id })}>
+          {book.name}
+        </li>
+      ))
+    );
+  }
+  render() {
+    return (
+      <div>
+        <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.selected} />
+      </div>
+    );
+  }
+}
+
+export default graphql(getBooksQuery)(BookList);
