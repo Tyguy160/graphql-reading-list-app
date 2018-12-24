0f9f74388b319fe02fb7659c32ceac09a9bab577
Tyler Maschino <Tylermaschino@gmail.com>
2018-12-24 14:54:54 -0500

    Updated backend

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect('mongodb://tyler:test123@ds243084.mlab.com:43084/gql-ninja');
 mongoose.connection.once('open', () => {
@@ -14,7 +17,7 @@ app.use(
   '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
   })
 );
 
